0d45c95b1fe833c723e7fee43f8e790ea1afe53a
8tone <logankingltd@gmail.com>
2018-03-09 00:04:27 -0500

    .

--- PharaohSlots/app/src/main/java/ca/georgebrown/game2011/mathgame/MainActivity.java
+++ PharaohSlots/app/src/main/java/ca/georgebrown/game2011/mathgame/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     TextView playerMoneyText;
     TextView playerBetText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
